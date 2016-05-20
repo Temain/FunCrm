ae7fd0e99fe6735e355095491e168a0d39b1e1f2
Temain <asv.artm@gmail.com>
2016-05-20 16:01:41 +0300

    Init

---
+++ FunCrm.Web/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FunCrm.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Tasks()
+        {
+            return PartialView("_Tasks");
+        }
+
+        public ActionResult Employees()
+        {
+            return PartialView("_Employees");
+        }
+
+        public ActionResult Analize()
+        {
+            return PartialView("_Analize");
+        }
+
+        public ActionResult Reports()
+        {
+            return PartialView("_Reports");
+        }
+    }
+}
---
+++ FunCrm.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FunCrm.Web.MvcApplication" Language="C#" %>
